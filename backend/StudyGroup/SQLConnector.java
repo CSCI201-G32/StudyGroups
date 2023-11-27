5cb140977f28b94c742108e7744de3ad31dda042
Krishna Biniwale <122494620+Krishna-Biniwale@users.noreply.github.com>
2023-11-26 20:10:05 -0800

    Update SQLConnector.java

--- backend/StudyGroup/SQLConnector.java
+++ backend/StudyGroup/SQLConnector.java
@@ -15,7 +15,7 @@ public class SQLConnector {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/joestabledb?user=root&password=root");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/StudyGroup?user=root&password=root");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
