501eab476a4095954611493b718cf3ed79c52f64
Brice Brown <BriceBro@usc.edu>
2023-11-30 00:45:36 -0800

    Fixed Account Issue

--- backend/StudyGroup/AccountServlet.java
+++ backend/StudyGroup/AccountServlet.java
@@ -20,17 +20,31 @@ public class AccountServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 	
+	protected void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    setAccessControlHeaders(response);
+	    response.setStatus(HttpServletResponse.SC_OK);
+	}
+
+	private void setAccessControlHeaders(HttpServletResponse response) {
+	    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+	    response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+	    response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	    response.setHeader("Access-Control-Allow-Credentials", "true");
+	}
+	
+	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        	doPost(request, response);
+	    setAccessControlHeaders(response);
+	    super.service(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-			
 		PrintWriter pw = response.getWriter();
-		response.setContentType("application/json");
+		response.setContentType("application/x-www-form-urlencoded");
 		response.setCharacterEncoding("UTF-8");
 		
 		String userId = request.getParameter("userId");
+		System.out.println(userId);
 		
 		Gson gson = new Gson();
 		
@@ -50,19 +64,21 @@ public class AccountServlet extends HttpServlet{
 			}
 			
 			if(info.isEmpty()) {
+				System.out.println("Failed to get info");
 				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 				String error = "Failed to get User Info";
 				pw.write(gson.toJson(error));
 				pw.flush();
 			}
 			else {
+				
 				response.setStatus(HttpServletResponse.SC_OK);
 				JsonObject json = new JsonObject();
 				json.addProperty("FirstName", info.get(0));
 				json.addProperty("LastName", info.get(1));
 				json.addProperty("Email", info.get(2));
 				json.addProperty("Major", info.get(3));
-				
+
 				pw.write(json.toString());
 				pw.flush();
 			}
--- frontend/src/features/ProfileManagement/Account.js
+++ frontend/src/features/ProfileManagement/Account.js
@@ -1,79 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import '../../assets/profile-management/Account.css';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const AccountPage = () => {
-  const [accountInfo, setAccountInfo] = useState(null);
+    const [accountInfo,
+        setAccountInfo] = useState(null);
 
-  useEffect(() => {
-    const getCookie = (cname) => {
-      const name = cname + '=';
-      const decodedCookie = decodeURIComponent(document.cookie);
-      const ca = decodedCookie.split(';');
-      for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-          c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-          return c.substring(name.length, c.length);
-        }
-      }
-      return '';
-    };
+    useEffect(() => {
+        const getCookie = (cname) => {
+            const name = cname + '=';
+            const decodedCookie = decodeURIComponent(document.cookie);
+            const ca = decodedCookie.split(';');
+            for (let i = 0; i < ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) === ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) === 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return '';
+        };
 
-    const displayInfo = () => {
-      const cookie = getCookie('UserID');
-      axios
-        .get('AccountServlet', {
-          params: {
-            userId: cookie,
-          },
-        })
-        .then((response) => {
-          const info = response.data;
-          setAccountInfo(info);
-        })
-        .catch((error) => {
-          console.error(error);
-          setAccountInfo({ error: 'Error fetching account information.' });
-        });
-    };
+        const displayInfo = () => {
+            const cookie = getCookie('UserID');
+            const url = 'http://localhost:8080/StudyGroups/AccountServlet';
+            const formData = new URLSearchParams();
+            formData.append('userId', cookie);
 
-    displayInfo();
-  }, []);
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: formData.toString()
+            }).then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            }).then(data => {
+                setAccountInfo(data);
+            }).catch(error => {
+                console.error(error);
+                setAccountInfo({error: 'Error fetching account information.'});
+            });
+        };
 
-  return (
-    <div>
-      <header>
+        displayInfo(); // This should be inside the useEffect but outside of displayInfo
+    }, []);
+
+    return (
         <div>
-          <h1>Account</h1>
+            <header>
+                <div>
+                    <h1>Account</h1>
+                </div>
+                <div>
+                    <Link to="/mainHomePage" className="fa fa-home"/>
+                    <Link to="/home" className="fa fa-users"/>
+                    <Link to="/search" className="fa fa-search"/>
+                    <Link to="/calendar" className="fa fa-calendar"/>
+                    <Link to="/account" className="fa fa-user-circle"/>
+                    <Link to="/" className="fa fa-sign-out"/>
+                </div>
+            </header>
+            <div id="accountMsg">
+                {accountInfo
+                    ? (
+                        <fieldset className="results">
+                            <legend>Account Information</legend>
+                            <p>First Name: {accountInfo.FirstName}</p>
+                            <p>Last Name: {accountInfo.LastName}</p>
+                            <p>Email: {accountInfo.Email}</p>
+                            <p>Major: {accountInfo.Major}</p>
+                        </fieldset>
+                    )
+                    : (
+                        <p>Loading...</p>
+                    )}
+            </div>
         </div>
-        <div>
-          <Link to="/mainHomePage" className="fa fa-home" />
-          <Link to="/home" className="fa fa-users" />
-          <Link to="/search" className="fa fa-search" />
-          <Link to="/calendar" className="fa fa-calendar" />
-          <Link to="/account" className="fa fa-user-circle" />
-          <Link to="/" className="fa fa-sign-out" />
-        </div>
-      </header>
-      <div id="accountMsg">
-        {accountInfo ? (
-          <fieldset className="results">
-            <legend>Account Information</legend>
-            <p>First Name: {accountInfo.FirstName}</p>
-            <p>Last Name: {accountInfo.LastName}</p>
-            <p>Email: {accountInfo.Email}</p>
-            <p>Major: {accountInfo.Major}</p>
-          </fieldset>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AccountPage;
