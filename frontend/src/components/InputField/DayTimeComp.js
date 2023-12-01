1aa89495f86d95cf462386c1ce48dce8103ef8b6
Brice Brown <BriceBro@usc.edu>
2023-11-30 18:55:15 -0800

    small bug fix with creating day and time

--- frontend/src/components/InputField/DayTimeComp.js
+++ frontend/src/components/InputField/DayTimeComp.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import DayTimeSelection from './DayTimeSelection';
 import '../../assets/study-group/DayTime.css'; 
-import MeetingTimes from '../../features/StudyGroupManagement/components/MeetingList'
+import MeetingList from '../../features/StudyGroupManagement/components/MeetingList'
 
 const DayTimeComp = ({ meetingTimes, onAddMeetingTime, onRemoveMeeting }) => {
   return (
     <div className="day-time-comp">
       <DayTimeSelection onAddSelection={onAddMeetingTime} />
 
-      <MeetingTimes meetingTimes={meetingTimes} onRemoveMeetingTime={onRemoveMeeting}/>
+      <MeetingList meetingTimes={meetingTimes} onRemoveMeetingTime={onRemoveMeeting}/>
     </div>
   );
 };
